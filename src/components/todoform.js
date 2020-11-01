84a7e236fe734232490b94c71f1adaf72ebbd29d
raghavendra-star <raghuraghaven6@gmail.com>
2020-11-01 21:50:38 +0530

    first commit

---
+++ src/components/todoform.js
@@ -0,0 +1,56 @@
+import React,{useState} from "react";
+import {
+    FormGroup,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    Button,
+    Form,
+    Container,
+} from "reactstrap";
+import {v4} from "uuid";
+
+const TodoForm=({addTodos})=>{
+    const [todoString,setTodoString]=useState("");
+
+    const handlesubmit=e=>{
+        e.preventDefault()
+        if(todoString===""){
+            return alert("please fill the value")
+        }
+        const todo={
+            todoString,
+            id:v4()
+        };
+        addTodos(todo);
+
+        setTodoString("");
+    }
+    return(
+        <Form onSubmit={handlesubmit}>
+            <FormGroup>
+                <InputGroup>
+                <Input
+                type="text"
+                name="todo"
+                id="todo"
+                placeholder="Enter a todo "
+                value={todoString}
+                onChange={e=>setTodoString(e.target.value)}
+                />
+                <InputGroupAddon addonType="prepend">
+                    <Button color="success">
+                        Add 
+                    </Button>
+                </InputGroupAddon>
+
+                
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    )
+
+}
+
+
+export default TodoForm;
